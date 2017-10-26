a5e7d7977e5b2cb7b4b5f1cd4fbc9166b9f40252
DIRZO1087 <ingjoseluisdirzo@hotmail.com>
2017-10-25 23:46:47 -0500

    Version Dirzo 1

--- app/src/main/java/appstosolutions/savinglives/MainActivity.java
+++ app/src/main/java/appstosolutions/savinglives/MainActivity.java
@@ -1,7 +1,12 @@
 package appstosolutions.savinglives;
 
 
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.content.Intent;
 import android.media.MediaPlayer;
+import android.os.Build;
+import android.provider.Settings;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.animation.Animation;
@@ -27,6 +32,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
 
     MediaPlayer mp;
     private Button boton_activar;
+    private NotificationManager mNotificationManager;
+    private Notification mNotification;
 
     private static final int MY_PERMISSIONS_REQUEST_SEND_SMS = 0;
 
@@ -35,6 +42,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mNotificationManager = (NotificationManager) getSystemService(NOTIFICATION_SERVICE);
+        changeInterruptionFiler(NotificationManager.INTERRUPTION_FILTER_ALL);
 
         if (ContextCompat.checkSelfPermission(this,
                 Manifest.permission.SEND_SMS)
@@ -42,6 +51,8 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
                 Manifest.permission.RECEIVE_SMS)
                 != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
                 Manifest.permission.READ_PHONE_STATE)
+                != PackageManager.PERMISSION_GRANTED || ContextCompat.checkSelfPermission(this,
+                Manifest.permission.ACCESS_NOTIFICATION_POLICY)
                 != PackageManager.PERMISSION_GRANTED) {
 
             ActivityCompat.requestPermissions(this,
@@ -71,6 +82,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
 
             findViewById(R.id.Fondoblanco).setVisibility(View.INVISIBLE);
             findViewById(R.id.button).setBackground(getDrawable(R.drawable.corazon_invertido));
+            changeInterruptionFiler(NotificationManager.INTERRUPTION_FILTER_NONE);
         } else {
             Animation slideUpIn = AnimationUtils.loadAnimation(MainActivity.this, R.anim.scale);
 
@@ -80,6 +92,7 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
 
             findViewById(R.id.Fondoblanco).setVisibility(View.VISIBLE);
             findViewById(R.id.button).setBackground(getDrawable(R.drawable.corazon_1));
+            changeInterruptionFiler(NotificationManager.INTERRUPTION_FILTER_ALL);
 
         }
         MediaPlayer mp = MediaPlayer.create(this, R.raw.latido);
@@ -87,6 +100,68 @@ public class MainActivity extends AppCompatActivity implements OnClickListener  
 
 
     }
+
+    protected void changeInterruptionFiler(int interruptionFilter){
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.M){ // If api level minimum 23
+            /*
+                boolean isNotificationPolicyAccessGranted ()
+                    Checks the ability to read/modify notification policy for the calling package.
+                    Returns true if the calling package can read/modify notification policy.
+                    Request policy access by sending the user to the activity that matches the
+                    system intent action ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS.
+
+                    Use ACTION_NOTIFICATION_POLICY_ACCESS_GRANTED_CHANGED to listen for
+                    user grant or denial of this access.
+
+                Returns
+                    boolean
+
+            */
+            // If notification policy access granted for this package
+            if(mNotificationManager.isNotificationPolicyAccessGranted()){
+                /*
+                    void setInterruptionFilter (int interruptionFilter)
+                        Sets the current notification interruption filter.
+
+                        The interruption filter defines which notifications are allowed to interrupt
+                        the user (e.g. via sound & vibration) and is applied globally.
+
+                        Only available if policy access is granted to this package.
+
+                    Parameters
+                        interruptionFilter : int
+                        Value is INTERRUPTION_FILTER_NONE, INTERRUPTION_FILTER_PRIORITY,
+                        INTERRUPTION_FILTER_ALARMS, INTERRUPTION_FILTER_ALL
+                        or INTERRUPTION_FILTER_UNKNOWN.
+                */
+
+                // Set the interruption filter
+                mNotificationManager.setInterruptionFilter(interruptionFilter);
+
+                if(interruptionFilter == NotificationManager.INTERRUPTION_FILTER_NONE) {
+                    //mNotification.flags = Notification.FLAG_AUTO_CANCEL;
+                }
+                if(interruptionFilter == NotificationManager.INTERRUPTION_FILTER_ALL){
+                    //mNotification.flags = Notification.FLAG_INSISTENT;
+                }
+
+            }else {
+                /*
+                    String ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS
+                        Activity Action : Show Do Not Disturb access settings.
+                        Users can grant and deny access to Do Not Disturb configuration from here.
+
+                    Input : Nothing.
+                    Output : Nothing.
+                    Constant Value : "android.settings.NOTIFICATION_POLICY_ACCESS_SETTINGS"
+                */
+                // If notification policy access not granted for this package
+                Intent intent = new Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS);
+                startActivity(intent);
+            }
+        }
+    }
+
 }
 
 
